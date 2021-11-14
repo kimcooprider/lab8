15d8677b2ae02372b161fcd7b7cf59b27700bd9d
kimcooprider <kimcooprider@gmail.com>
2021-11-14 14:09:00 -0600

    updating code for assignment

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('app.js is connected');
 
 
-// let welcomeMessage = alert('Hello Corgi Enthusiast');
+let welcomeMessage = alert('Hello Corgi Enthusiast');
 let getNum = true;
 
 while(getNum){
@@ -14,35 +14,78 @@ while(getNum){
     }
  }
 
+//  function howManyCorgi(){
+
+//   let greeting;
+  
+//   if(corgiNumParsed < 1){
+//     greeting = 'You should get a Corgi immediately.';
+//   } else if(corgiNumParsed === 1){
+//     greeting = 'Good job!  Corgis Rock!';
+//   } else if(corgiNumParsed !== 0 && corgiNumParsed === 5){
+//     greeting = 'Good you have atleast one and 5.';
+//   } else if(corgiNumParsed === 100 || corgiNumParsed === 50){
+//     greeting = 'you should share more.';
+//   } else {
+//     greeting ='Get yourseld a Corgi, soon.';
+//   }
+//   console.log('this is the greeting ',greeting);
+//   document.write(greeting);
+  
+//   }
+//   howManyCorgi();
+ 
+ 
+ 
+ 
+//  let imageElement = document.getElementById('imageSection');
+
+
+//  let userInput = prompt('How many images would you like to see?');
+//  console.log('Our user input is of type: ',typeof(userInput));
+ 
+ 
+//  let userInputParsed = parseInt(userInput);
+//  console.log('user input parsed to int: ', userInputParsed)
+ 
+//  if(!isNaN(userInputParsed)){
+//    console.log('we have a valid number for input');
+//  } else {
+//    userInputParsed = parseInt(prompt('How many images? Enter a Number.'));
+//  }
+//  console.table(typeof(userInput));
+
+
+//  for(let i = 0; i < userInputParsed; i++){
+//   // console.log('proof of life', i);
+//   let listImage = document.createElement('li');
+//   listImage.textContent = `image number ${i + 1}`;
+//   let image = document.createElement('img');
+//   image.setAttribute('src', 'images/codeImage.png');
+//   listImage.appendChild(image);
+//   console.log(listImage);
+//   imageElement.appendChild(listImage);
+// }
+
+
+// function  getCorgiNum(){
+//     let greeting;
+
+//     if(corgiNum < 2 && corgiNum > 0){
+//     greeting = 'Good job!  Corgis Rock!';
+//     } else if(corgiNum === 3 || corgiNum < 5){
+//     greeting = 'You are on your way!';
+//     } else if (corgiNum >= 5){
+//     greeting = 'Wow, that is a lot of corgis!';
+//     }  else {  
+//     greeting = 'You should get a Corgi immediately.';
+//     }
+//     document.write(greeting);
 
 
 
 
-
-
-
-// console.log('User has this number of corgis',corgiNum);
-
-
-function  getCorgiNum(){
-    let greeting;
-
-    if(corgiNum < 2 && corgiNum > 0){
-    greeting = 'Good job!  Corgis Rock!';
-    } else if(corgiNum === 3 || corgiNum < 5){
-    greeting = 'You are on your way!';
-    } else if (corgiNum >= 5){
-    greeting = 'Wow, that is a lot of corgis!';
-    }  else {  
-    greeting = 'You should get a Corgi immediately.';
-    }
-    document.write(greeting);
-
-
-
-
-console.log('proof of life');
-}
+// console.log('proof of life');}
 
 
 // getCorgiNum();
